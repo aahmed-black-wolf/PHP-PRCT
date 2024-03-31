3a8c5205c430ac9ffc94cc511d9c63dec9ceb10d
aahmed <abdo.eslam20021@gmail.com>
2024-03-31 02:46:03 +0200

    [UPD] append form validation append data for database [#6]

--- controllers/createNote.php
+++ controllers/createNote.php
@@ -1,9 +1,38 @@
 <?php
      
      $heading = "Create Note";
-         
-    
-     dd($_SERVER);
+              
+     $config = require("./config.php");
+
+     $db = new Database($config["database"]);
+       
+
+
+     if($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $errors = [];
+   
+      if(strlen($_POST["title"]) === 0){
+         $errors['error'] = "Please enter title";
+       }
+  
+       if(strlen($_POST["body"]) === 0){
+        $errors['error'] = "Please enter body";
+      }
+
+      if(strlen($_POST["title"]) > 10 || strlen($_POST["body"]) > 1000 ){
+         $errors['error'] = "Please enter title from 0  to 10 and body from 0 to 1000";
+       }
+  
+       
+      if(empty($errors)){
+           $db->query('INSERT INTO notes (body,title,user_id) VALUES (:body,:title,:user_id)',[
+              'body'=> $_POST['body'],
+              'title'=> $_POST['title'],
+              'user_id'=> 1,
+           ]);
+      }
+     }
+
 
       
 
--- views/createNote.view.php
+++ views/createNote.view.php
@@ -3,11 +3,17 @@
 <?php require("components/banner.php")?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <form method="GET" class="flex flex-col  gap-3">
+        <form method="POST" class="flex flex-col  gap-3">
+            <input type="text" name="title" id="title"
+                class="focus:outline-[lightcoral] p-[30px] text-[30px] rounded-md">
             <textarea name="body" class="focus:outline-[lightcoral] p-[30px] text-[30px] rounded-md" name="" id=""
-                cols="30" rows="10"></textarea>
+                cols="30" rows="10"><?= $_POST['body'] ?? '' ?> </textarea>
+            <?php if(isset($errors['error'])) : ?>
+            <p class="text-[red] text-xl"><?= $errors['error'];?></p>
+            <?php endif;?>
             <button
                 class="px-[30px] py-[10px] font-[600] text-[20px] bg-[lightcoral]  text-white rounded-md">Create</button>
+
         </form>
     </div>
 </main>
